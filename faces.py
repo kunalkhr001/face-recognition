0b5c3f7dbaf2eb803fe0d220f9fbc59a290a4482
kunalkhr001 <32545135+kunalkhr001@users.noreply.github.com>
2018-08-09 17:03:29 +0530

    Update faces.py

--- faces.py
+++ faces.py
@@ -19,20 +19,17 @@ print (labels)
 cap = cv2.VideoCapture(0)
 
 while(True):
-    # Capture frame-by-frame
     ret, frame = cap.read()
     gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
     for (x, y, w, h) in faces:
     	#print(x,y,w,h)
-    	roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
+    	roi_gray = gray[y:y+h, x:x+w] 
     	roi_color = frame[y:y+h, x:x+w]
 
-    	# recognize? deep learned model predict keras tensorflow pytorch scikit learn
     	id_, conf = recognizer.predict(roi_gray)
     	if conf>=1 and conf <= 100:
-    		#print(5: #id_)
-    		#print(labels[id_])
+		
     		font = cv2.FONT_HERSHEY_SIMPLEX
     		name = labels[id_]
     		color = (255, 255, 255)
@@ -42,16 +39,15 @@ while(True):
     	img_item = "new.png"
     	cv2.imwrite(img_item, roi_color)
 
-    	color = (255, 0, 0) #BGR 0-255 
+    	color = (255, 0, 0) 
     	stroke = 2
     	end_cord_x = x + w
     	end_cord_y = y + h
     	cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
-    	    # Display the resulting frame
+    	    
     cv2.imshow('frame',frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
